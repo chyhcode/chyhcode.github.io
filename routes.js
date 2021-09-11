ca91196c85d5dfa6556526ea230bd36d0df5b3c4
chyhcode <chyhcode@gmail.com>
2021-09-11 20:27:36 +0800

    Update routes.js

--- routes.js
+++ routes.js
@@ -14,4 +14,5 @@ var routes = {
 // 從 routes 中找到該關鍵字所對應到的網址
 var url = routes[location.pathname.replace('/')];
 // 若該關鍵字有設定對應的網址，則轉跳到該網址中，若沒有則回到首頁
+alert(url);
 location.href = url ? url : 'https://chyhcode.github.io';
