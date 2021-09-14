2de23e9e1b3664714552af24adad5010ffd0f623
chyhcode <chyhcode@gmail.com>
2021-09-14 23:45:12 +0800

    Update bookmarklets.js

--- NTUH-tools/bookmarklets.js
+++ NTUH-tools/bookmarklets.js
@@ -1,11 +1,10 @@
 // https://cdn.jsdelivr.net/gh/chyhcode/chyhcode.github.io@main/NTUH-tools/bookmarklets.js
 var bookmarklet = {
-	openLink: [
-	{
+	openLink: [{
 		name: 'ConfirmDiagnosisOrder',
 		title: '主',
 		description: '',
-		CommonUsed: 0,
+		CommonUsed: 1,
 		func: function() {
 			open('http://ihisaw.ntuh.gov.tw/WebApplication/InPatient/Ward/ConfirmDiagnosisOrder.aspx' + location.search, 'ConfirmDiagnosisOrder');
 		},
@@ -21,7 +20,7 @@ var bookmarklet = {
 		name: 'PatientMedicalRecordListQuery',
 		title: '史',
 		description: '',
-		CommonUsed: 0,
+		CommonUsed: 1,
 		func: function() {
 			open('http://ihisaw.ntuh.gov.tw/WebApplication/OtherIndependentProj/PatientBasicInfoEdit/PatientMedicalRecordListQuery.aspx' + location.search, 'PatientMedicalRecordListQuery');
 		},
@@ -29,7 +28,7 @@ var bookmarklet = {
 		name: 'MedicationHistory',
 		title: '藥史',
 		description: '',
-		CommonUsed: 0,
+		CommonUsed: 1,
 		func: function() {
 			open('http://ihisaw.ntuh.gov.tw/WebApplication/OtherIndependentProj/MedicationHistory/Default.aspx' + location.search, 'MedicationHistory');
 		},
@@ -69,7 +68,7 @@ var bookmarklet = {
 		name: 'VitalSign_TPR',
 		title: '生',
 		description: '',
-		CommonUsed: 1,
+		CommonUsed: 2,
 		func: function() {
 			open('http://ihisaw.ntuh.gov.tw/WebApplication/InPatient/Nursing/VitalSign_TPR.aspx' + location.search, 'VitalSign_TPR');
 		},
@@ -77,7 +76,7 @@ var bookmarklet = {
 		name: 'DrugGivenNote',
 		title: '給藥紀錄',
 		description: '',
-		CommonUsed: 0,
+		CommonUsed: 1,
 		func: function() {
 			open('http://ihisaw.ntuh.gov.tw/WebApplication/InPatient/Ward/DrugGivenNote.aspx' + location.search, 'DrugGivenNote');
 		},
@@ -85,7 +84,7 @@ var bookmarklet = {
 		name: 'CatheterCare',
 		title: '管路',
 		description: '',
-		CommonUsed: 0,
+		CommonUsed: 1,
 		func: function() {
 			open('http://ihisaw.ntuh.gov.tw/WebApplication/InPatient/Nursing/CatheterCare.aspx' + location.search, 'CatheterCare');
 		},
@@ -93,7 +92,7 @@ var bookmarklet = {
 		name: 'Estimate_IO_Summery',
 		title: 'IO',
 		description: '',
-		CommonUsed: 0,
+		CommonUsed: 1,
 		func: function() {
 			open('http://ihisaw.ntuh.gov.tw/WebApplication/InPatient/Nursing/Estimate_IO_Summery.aspx' + location.search, 'Estimate_IO_Summery');
 		},
@@ -117,7 +116,7 @@ var bookmarklet = {
 		name: 'NursingProgressNote',
 		title: '護程',
 		description: '',
-		CommonUsed: 0,
+		CommonUsed: 1,
 		func: function() {
 			open('http://ihisaw.ntuh.gov.tw/WebApplication/InPatient/Nursing/NursingProgressNote.aspx' + location.search, 'NursingProgressNote');
 		},
@@ -125,7 +124,7 @@ var bookmarklet = {
 		name: 'OffDutyNurV2',
 		title: '護交',
 		description: '',
-		CommonUsed: 1,
+		CommonUsed: 2,
 		func: function() {
 			open('http://ihisaw.ntuh.gov.tw/WebApplication/InPatient/Ward/OffDutyNurV2.aspx' + location.search, 'OffDutyNurV2');
 		},
@@ -181,7 +180,7 @@ var bookmarklet = {
 		name: 'MedicalReportContent',
 		title: '報',
 		description: '',
-		CommonUsed: 1,
+		CommonUsed: 2,
 		func: function() {
 			open('http://ihisaw.ntuh.gov.tw/WebApplication/ElectronicMedicalReportViewer/MedicalReportContent.aspx' + location.search, 'MedicalReportContent');
 		},
@@ -189,7 +188,7 @@ var bookmarklet = {
 		name: 'PACSImageShowList2',
 		title: 'PACS',
 		description: '',
-		CommonUsed: 1,
+		CommonUsed: 2,
 		func: function() {
 			open('http://ihisaw.ntuh.gov.tw/WebApplication/OtherIndependentProj/PatientBasicInfoEdit/PACSImageShowList2.aspx' + location.search, 'PACSImageShowList2');
 		},
@@ -221,7 +220,7 @@ var bookmarklet = {
 		name: 'InsertProgressNoteContent',
 		title: '程',
 		description: '',
-		CommonUsed: 1,
+		CommonUsed: 2,
 		func: function() {
 			open('http://ihisaw.ntuh.gov.tw/WebApplication/InPatient/Ward/InsertProgressNoteContent.aspx' + location.search, 'InsertProgressNoteContent');
 		},
@@ -237,7 +236,7 @@ var bookmarklet = {
 		name: 'DoctorEnterOrder',
 		title: '診',
 		description: '',
-		CommonUsed: 1,
+		CommonUsed: 2,
 		func: function() {
 			open('http://ihisaw.ntuh.gov.tw/WebApplication/InPatient/Ward/DoctorEnterOrder.aspx' + location.search, 'DoctorEnterOrder');
 		},
@@ -245,7 +244,7 @@ var bookmarklet = {
 		name: 'MedicationV2',
 		title: '處',
 		description: '',
-		CommonUsed: 1,
+		CommonUsed: 2,
 		func: function() {
 			open('http://ihisaw.ntuh.gov.tw/WebApplication/InPatient/Ward/MedicationV2.aspx' + location.search, 'MedicationV2');
 		},
@@ -261,7 +260,7 @@ var bookmarklet = {
 		name: 'NotifyOtherDoctor',
 		title: '照',
 		description: '',
-		CommonUsed: 0,
+		CommonUsed: 1,
 		func: function() {
 			open('http://ihisaw.ntuh.gov.tw/WebApplication/InPatient/Ward/NotifyOtherDoctor.aspx' + location.search, 'NotifyOtherDoctor');
 		},
@@ -269,7 +268,7 @@ var bookmarklet = {
 		name: 'BloodStockCreateOrder',
 		title: '血',
 		description: '',
-		CommonUsed: 0,
+		CommonUsed: 1,
 		func: function() {
 			open('http://ihisaw.ntuh.gov.tw/WebApplication/BloodStock/Pages/His/CreateOrder.aspx' + location.search, 'BloodStockCreateOrder');
 		},
@@ -277,7 +276,7 @@ var bookmarklet = {
 		name: 'SimpleShowiframePage',
 		title: 'EMR',
 		description: '',
-		CommonUsed: 0,
+		CommonUsed: 1,
 		func: function() {
 			open('http://ihisaw.ntuh.gov.tw/WebApplication/OtherIndependentProj/PatientBasicInfoEdit/SimpleShowiframePage.aspx' + location.search, 'SimpleShowiframePage');
 		},
@@ -357,12 +356,115 @@ var bookmarklet = {
 		name: 'EBoardMainPage',
 		title: '電子白板',
 		description: '',
-		CommonUsed: 0,
+		CommonUsed: 1,
 		func: function() {
 			open('http://ihisaw.ntuh.gov.tw/WebApplication/EBoard/EBoardMainPage.aspx' + location.search, 'EBoardMainPage');
 		},
 	}, ],
-	special: [],
+	special: [{
+		name: 'DiagCertificate',
+		title: '診斷證明書小工具',
+		description: '在診斷書開立頁面使用，將急診、住院、手術日期變成民國年，附帶常用語句"病患因上述疾病"、"至本院急診就診並留觀待床"、"轉至一般病房住院"...等等片語，以及並且支援點擊帶入文字。須注意，急診就診時間須點開"病人急診歷史動向"中的"病歷號"展開幾分幾秒。若小工具因為portal刷新被洗掉，例如使用系統內建片語、展開急診動向等，再重新使用小工具即可。若在其他頁面使用，將自動跳轉至診斷書頁面。',
+		func: function() {
+			if (location.pathname != '/WebApplication/Clinics/DiagCertificate_New.aspx')
+				open('http://hisaw.ntuh.gov.tw/WebApplication/Clinics/DiagCertificate_New.aspx' + location.search, 'DiagCertificate_New');
+			else if (document.getElementById('NTUHWeb1_InstructionSetItem')) {
+				if (window.NodeList && !NodeList.prototype.forEach) {
+					NodeList.prototype.forEach = Array.prototype.forEach;
+				}
+
+				function qFor(s, f) {
+					document.querySelectorAll(s).forEach(f);
+				}
+
+				function nn(n) {
+					return n < 10 ? '0' + n : '' + n;
+				}
+
+				function dateStr(d, addTime) {
+					d = new Date(d);
+					return '於民國' + (d.getFullYear() - 1911) + '年' + nn(d.getMonth() + 1) + '月' + nn(d.getDate()) + '日' + (addTime ? nn(d.getHours()) + '時' + nn(d.getMinutes()) + '分' : '');
+				}
+
+				function createLink(wrap, t) {
+					var a = document.createElement('a');
+					a.innerText = t;
+					a.style = 'font-weight: bold; font-size: 16px;';
+					a.href = 'javascript: document.getElementById(\"NTUHWeb1_InstructionSetItem\").value += \"' + t + '\"; undefined;';
+					wrap.innerText = '';
+					wrap.bmklet = true;
+					return wrap.appendChild(a);
+				}
+
+				var WDtxt = ['至本院一般病房住院，', '轉至一般病房住院，', '轉至加護病房，', '轉至一般病房，'],
+					ref = document.querySelector('#NTUHWeb1_gvwStatusHistory tr:nth-child(2) > td:nth-child(4)');
+				ref && !ref.bmklet && createLink(ref, dateStr(ref.innerText, true) + '至本院急診就診並留觀待床，');
+
+				qFor('#NTUHWeb1_dgLogPatTransferBed [class^=\"tableText\"] td:nth-child(3)', function(td) {
+					!td.bmklet && createLink(td, dateStr(td.innerText));
+				});
+
+				ref = new Date();
+				qFor('#NTUHWeb1_dgOpScheduleData tr[class^=\"tableText\"] td:nth-child(1)', function(td) {
+					!td.bmklet && createLink(td, dateStr(td.innerText.length == 5 ? (ref.getFullYear() + '/' + td.innerText) : td.innerText) + '接受[術式]，');
+				});
+
+				qFor('[id$=\"lblChartNo\"]', function(a) {
+					a.innerText = '展開檢傷時間';
+					a.style = 'font-weight: bold; font-size: 16px;';
+				});
+
+				ref.setDate(ref.getDate() + 1);
+				WDtxt.push(dateStr(ref) + '出院，出院後宜持續門診追蹤治療。');
+				WDtxt.forEach(function(t) {
+					document.getElementById('NTUHWeb1_dgLogPatTransferBed').parentNode.appendChild(createLink(document.createElement('div'), t).parentNode);
+				});
+
+				document.getElementById('NTUHWeb1_InstructionSetItem').value = '病患因上述疾病，';
+				alert('診斷書小工具已開啟!');
+			}
+			else alert('此畫面非診斷書開立畫面!');
+		},
+		funcStr: "javascript: if(location.pathname!='/WebApplication/Clinics/DiagCertificate_New.aspx')open('http://hisaw.ntuh.gov.tw/WebApplication/Clinics/DiagCertificate_New.aspx'+location.search,'DiagCertificate_New');else if(document.getElementById('NTUHWeb1_InstructionSetItem')){if(window.NodeList&&!NodeList.prototype.forEach){NodeList.prototype.forEach=Array.prototype.forEach;}function qFor(s,f){document.querySelectorAll(s).forEach(f);}function nn(n){return n<10?'0'+n:''+n;}function dateStr(d,addTime){d=new Date(d);return'於民國'+(d.getFullYear()-1911)+'年'+nn(d.getMonth()+1)+'月'+nn(d.getDate())+'日'+(addTime?nn(d.getHours())+'時'+nn(d.getMinutes())+'分':'');}function createLink(wrap,t){var a=document.createElement('a');a.innerText=t;a.style='font-weight: bold; font-size: 16px;';a.href='javascript: document.getElementById(\"NTUHWeb1_InstructionSetItem\").value += \"'+t+'\"; undefined;';wrap.innerText='';wrap.bmklet=true;return wrap.appendChild(a);}var WDtxt=['至本院一般病房住院，','轉至一般病房住院，','轉至加護病房，','轉至一般病房，'],ref=document.querySelector('#NTUHWeb1_gvwStatusHistory tr:nth-child(2) > td:nth-child(4)');ref&&!ref.bmklet&&createLink(ref,dateStr(ref.innerText,true)+'至本院急診就診並留觀待床，');qFor('#NTUHWeb1_dgLogPatTransferBed [class^=\"tableText\"] td:nth-child(3)',function(td){!td.bmklet&&createLink(td,dateStr(td.innerText));});ref=new Date();qFor('#NTUHWeb1_dgOpScheduleData tr[class^=\"tableText\"] td:nth-child(1)',function(td){!td.bmklet&&createLink(td,dateStr(td.innerText.length==5?(ref.getFullYear()+'/'+td.innerText):td.innerText)+'接受[術式]，');});qFor('[id$=\"lblChartNo\"]',function(a){a.innerText='展開檢傷時間';a.style='font-weight: bold; font-size: 16px;';});ref.setDate(ref.getDate()+1);WDtxt.push(dateStr(ref)+'出院，出院後宜持續門診追蹤治療。');WDtxt.forEach(function(t){document.getElementById('NTUHWeb1_dgLogPatTransferBed').parentNode.appendChild(createLink(document.createElement('div'),t).parentNode);});document.getElementById('NTUHWeb1_InstructionSetItem').value='病患因上述疾病，';alert('診斷書小工具已開啟!');}else alert('此畫面非診斷書開立畫面!');",
+	}, {
+		name: 'LongGreenSheet',
+		title: '綠單不換行小工具',
+		description: '在綠單使用，原本八格會換行的綠單，導致剛好跨行的資料不好看，此小工具會將綠單合併成一長串。',
+		func: function() {
+			var d, fs, t, i, j, k;
+			d = document.getElementById('Reportifrm');
+			if (d) {
+				d.style.height = '90vh';
+				d = d.contentDocument;
+			}
+			else d = document;
+
+			fs = d.querySelectorAll('fieldset.GroupHead');
+			for (k = 0; k < fs.length; k++) {
+				t = fs[k].querySelectorAll('table.GreenSheet');
+				for (var i = 1; i < t.length; i++) {
+					for (var j = 0; j < t[i].rows.length; j++) {
+						if (j != 1) t[i].rows[j].removeChild(t[i].rows[j].firstElementChild);
+						t[0].rows[j].innerHTML += t[i].rows[j].innerHTML;
+					}
+					fs[k].removeChild(t[i]);
+				}
+				fs[k].appendChild(d.createElement('div')).appendChild(t[0]);
+			}
+
+			if (!d.getElementById('GreenSheetOverflow')) {
+				s = d.createElement('style');
+				s.id = 'GreenSheetOverflow';
+				s.innerText = '.GroupHead{min-inline-size:unset}.GroupHead>div{overflow-x:auto}.GreenSheetTitle,.GreenSheet{table-layout:fixed;margin:0}.GreenSheet th{min-width:87px}.GreenSheet tbody tr>:first-child,.GreenSheet .LEFTTOP{min-width:150px;position:sticky;left:0}';
+				d.body.appendChild(s);
+			}
+
+			d = d.getElementById('rReportTab_lsvReportBody_ctrl0_ctl00_ReportTitle1_contentTemplatePlaceholder');
+			if (d) d.style.border = 'none';
+			undefined;
+		},
+		funcStr: "javascript: var d,fs,t,i,j,k;d=document.getElementById('Reportifrm');if(d){d.style.height='90vh';d=d.contentDocument;}else d=document;fs=d.querySelectorAll('fieldset.GroupHead');for(k=0;k<fs.length;k++){t=fs[k].querySelectorAll('table.GreenSheet');for(var i=1;i<t.length;i++){for(var j=0;j<t[i].rows.length;j++){if(j!=1)t[i].rows[j].removeChild(t[i].rows[j].firstElementChild);t[0].rows[j].innerHTML+=t[i].rows[j].innerHTML;}fs[k].removeChild(t[i]);}fs[k].appendChild(d.createElement('div')).appendChild(t[0]);}if(!d.getElementById('GreenSheetOverflow')){s=d.createElement('style');s.id='GreenSheetOverflow';s.innerText='.GroupHead{min-inline-size:unset}.GroupHead>div{overflow-x:auto}.GreenSheetTitle,.GreenSheet{table-layout:fixed;margin:0}.GreenSheet th{min-width:87px}.GreenSheet tbody tr>:first-child,.GreenSheet .LEFTTOP{min-width:150px;position:sticky;left:0}';d.body.appendChild(s);}d=d.getElementById('rReportTab_lsvReportBody_ctrl0_ctl00_ReportTitle1_contentTemplatePlaceholder');if(d)d.style.border='none';undefined;",
+	}, ],
 };
 
 function bookmarkletify(s) {
