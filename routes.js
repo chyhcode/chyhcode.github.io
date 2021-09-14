f36a7ad1b35ccc0ae3dfb0d552e5d7babad8ac47
chyhcode <chyhcode@gmail.com>
2021-09-14 23:45:43 +0800

    Update routes.js

--- routes.js
+++ routes.js
@@ -2,11 +2,11 @@ var routes = {
 	"g": "https://www.google.com.tw/",
 	"s": "https://drive.google.com/drive/folders/0B2w1u6MIh8tjdUNldHQxQkcxVFE",
 	"p": "http://portal.ntuh.gov.tw/",
-	"b": "https://chyhcode.github.io/NTUH-tools/BookmarkletCreator/BookmarkletCreator.html",
+	"b": "https://chyhcode.github.io/NTUH-tools/bookmarklet/bookmarklet.html",
 
 	"google": "https://www.google.com.tw/",
 	"starred": "https://drive.google.com/drive/starred",
 	"shared": "https://drive.google.com/drive/folders/0B2w1u6MIh8tjdUNldHQxQkcxVFE",
 	"portal": "http://portal.ntuh.gov.tw/",
-	"bookmarklet": "https://chyhcode.github.io/NTUH-tools/BookmarkletCreator/BookmarkletCreator.html"
+	"bookmarklet": "https://chyhcode.github.io/NTUH-tools/bookmarklet/bookmarklet.html"
 };
