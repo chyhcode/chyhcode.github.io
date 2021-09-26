c30cda671c9e47eaba8bb9de91c703a2cd3e89dc
chyhcode <chyhcode@gmail.com>
2021-09-27 01:11:40 +0800

    Create ntuh-tool-specifications.js

---
+++ ntuh-tools/ntuh-tool-specifications.js
@@ -0,0 +1,11 @@
+function foo() {
+	// javascript:
+
+	// undefined;
+}
+bookmarklet = {
+	name: 'foo',
+	title: 'foo小工具',
+	description: '在XXX頁面使用，',
+	path: "https://cdn.jsdelivr.net/gh/chyhcode/chyhcode.github.io@main/ntuh-tools/bookmarklet/bookmarklets.js",
+};
